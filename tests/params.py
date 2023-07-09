bbc52d4a8604fdcd32230b296177e0051f92c455
sanggusti <gustiowinata0@gmail.com>
2023-07-09 18:42:50 +0200

    Add test and several workflow regarding tests
    - smoke test for end to end training test
    - metric test

---
+++ tests/params.py
@@ -0,0 +1,5 @@
+WANDB_PROJECT = "mlops-course-001"
+ENTITY = None
+BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
+RAW_DATA_AT = 'bdd_simple_1k'
+PROCESSED_DATA_AT = 'bdd_simple_1k_split'
